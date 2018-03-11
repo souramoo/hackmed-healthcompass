3397e5a71fd6b8eb74b3844a4ddefad068328067
Souradip Mookerjee <souramoo@gmx.com>
2018-03-11 09:39:28 +0000

    Moar bugfixes, added confirmation screen

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ import datetime
 import pytz
 import json
 
+flags = []
+medcentres = [('21akti4jb49iv29jiuf0mjr5p8@group.calendar.google.com', 'Porter Brook Medical Centre', 'ChIJpZPvtGSCeUgRaWE7aMvStvg'), ('fduj2a3a0mdrrmtpi8en0jemsk@group.calendar.google.com', 'University Of Sheffield Health Centre', 'ChIJ_cbxtHiCeUgRK_eX8gO8cqs')]
 
 app = Flask(__name__)
 
@@ -22,36 +24,42 @@ def hello2():
 
 @app.route("/p/<problem>")
 def hello(problem):
-    r = requests.get('https://www.nhs.uk/Search/?q='+problem)
-    p = re.compile("<li data-fb-result=https://www.nhs.uk/conditions/(.+?)>")
-    uu = "https://www.nhs.uk/conditions/" + p.search(r.content.decode("utf-8")).group(1)
-    r = requests.get(uu)
-    if "pharmacist can help" in r.content.decode("utf-8"):
-        return "1"
-    else:
-        return "0"
+    try:
+        r = requests.get('https://www.nhs.uk/Search/?q='+problem)
+        p = re.compile("<li data-fb-result=https://www.nhs.uk/conditions/(.+?)>")
+        uu = "https://www.nhs.uk/conditions/" + p.search(r.content.decode("utf-8")).group(1)
+        r = requests.get(uu)
+        if "pharmacist can help" in r.content.decode("utf-8"):
+            return "1"
+        else:
+            return "0"
+    except:
+        return "0 error"
         
 @app.route("/wi/<problem>")
 def wi(problem):
-    bias = 0
-    r = requests.get('https://www.nhs.uk/Search/?q='+problem)
-    p = re.compile("<li data-fb-result=https://www.nhs.uk/conditions/(.+?)>")
-    uu = "https://www.nhs.uk/conditions/" + p.search(r.content.decode("utf-8")).group(1)
-    r = requests.get(uu)
-    print(uu)
-    if "walk-in" in r.content.decode("utf-8"):
-        bias = 1
-    else:
-        r = requests.get('https://www.nhs.uk/NHSEngland/AboutNHSservices/Emergencyandurgentcareservices/Pages/Walk-incentresSummary.aspx')
-        if urllib.parse.unquote(problem.lower()) in r.content.decode("utf-8").lower():
+    try:
+        bias = 0
+        r = requests.get('https://www.nhs.uk/Search/?q='+problem)
+        p = re.compile("<li data-fb-result=https://www.nhs.uk/conditions/(.+?)>")
+        uu = "https://www.nhs.uk/conditions/" + p.search(r.content.decode("utf-8")).group(1)
+        r = requests.get(uu)
+        print(uu)
+        if "walk-in" in r.content.decode("utf-8"):
             bias = 1
-    if bias == 1:
-        return "1"
-    else:
-        return "0"
+        else:
+            r = requests.get('https://www.nhs.uk/NHSEngland/AboutNHSservices/Emergencyandurgentcareservices/Pages/Walk-incentresSummary.aspx')
+            if urllib.parse.unquote(problem.lower()) in r.content.decode("utf-8").lower():
+                bias = 1
+        if bias == 1:
+            return "1"
+        else:
+            return "0"
+    except:
+        return "0 error"
         
 #import cal
-SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+SCOPES = 'https://www.googleapis.com/auth/calendar'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Google Calendar API Python Quickstart'
 
@@ -68,10 +76,7 @@ def get_credentials():
     if not credentials or credentials.invalid:
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
         flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
+        credentials = tools.run_flow(flow, store, {"logging_level": "DEBUG"})
         print('Storing credentials to ' + credential_path)
     return credentials
 
@@ -90,33 +95,67 @@ def freetimes():
     
     a = []
     
-    for (cal, calname, pid) in [('21akti4jb49iv29jiuf0mjr5p8@group.calendar.google.com', 'Porter Brook Medical Centre', 'ChIJpZPvtGSCeUgRaWE7aMvStvg')]:
+    for (cal, calname, pid) in medcentres:
+        print(calname)
         tz = pytz.timezone('Europe/London')
-        now = datetime.datetime.now() + datetime.timedelta(days=1)
-        the_datetime = tz.localize(datetime.datetime(now.year, now.month, now.day, 9, 0))
-        the_datetime2 = tz.localize(datetime.datetime(now.year, now.month, now.day, 17, 0))
-        body = {
-            "timeMin": the_datetime.isoformat(),
-            "timeMax": the_datetime2.isoformat(),
-            "timeZone": 'Europe/London',
-            "items": [{"id": cal}]
-           }
-        eventsResult = service.freebusy().query(body=body).execute()
-        cal_dict = eventsResult[u'calendars']
-        i = 0
-        while i < len(cal_dict[cal]['busy']):
-            st = datetime.datetime.strptime(cal_dict[cal]['busy'][i]["end"], "%Y-%m-%dT%H:%M:%SZ")
-            try:
-                en = datetime.datetime.strptime(cal_dict[cal]['busy'][i+1]["start"], "%Y-%m-%dT%H:%M:%SZ")
-            except:
-                en = the_datetime2.replace(tzinfo=None)
-            j = datetime.timedelta(0)
-            while st + j < en:
-                tdt = st + j
-                a.append({"name": calname, "time": tdt.strftime("%Y-%m-%dT%H:%M:%SZ"), "place_id": pid})
-                j += datetime.timedelta(minutes=10)
-            i += 1
+        dd = 0
+        while dd < 2:
+            now = datetime.datetime.now() + datetime.timedelta(days=dd)
+            dd += 1
+            the_datetime = tz.localize(datetime.datetime(now.year, now.month, now.day, 9, 0))
+            the_datetime2 = tz.localize(datetime.datetime(now.year, now.month, now.day, 17, 0))
+            body = {
+                "timeMin": the_datetime.isoformat(),
+                "timeMax": the_datetime2.isoformat(),
+                "timeZone": 'Europe/London',
+                "items": [{"id": cal}]
+               }
+            eventsResult = service.freebusy().query(body=body).execute()
+            cal_dict = eventsResult[u'calendars']
+            i = 0
+            while i < len(cal_dict[cal]['busy']):
+                st = datetime.datetime.strptime(cal_dict[cal]['busy'][i]["end"], "%Y-%m-%dT%H:%M:%SZ")
+                try:
+                    en = datetime.datetime.strptime(cal_dict[cal]['busy'][i+1]["start"], "%Y-%m-%dT%H:%M:%SZ")
+                except:
+                    en = the_datetime2.replace(tzinfo=None)
+                j = datetime.timedelta(0)
+                while st + j < en:
+                    tdt = st + j
+                    a.append({"name": calname, "time": tdt.strftime("%Y-%m-%dT%H:%M:%SZ"), "place_id": pid, "calid": cal})
+                    j += datetime.timedelta(minutes=10)
+                i += 1
     return json.dumps(a)
+    
+@app.route("/book/<calid>/<time>/<complaint>/")
+def book(time, calid, complaint):
+    name = "Souradip"
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http)
+    event = {
+        'summary': name + ' appointment',
+        'location': '',
+        'description': complaint,
+        'start': {
+        'dateTime': time,
+        'timeZone': 'Europe/London',
+        },
+        'end': {
+        'dateTime': (datetime.datetime.strptime(time, "%Y-%m-%dT%H:%M:%SZ") + datetime.timedelta(minutes=10)).strftime("%Y-%m-%dT%H:%M:%SZ"),
+        'timeZone': 'Europe/London',
+        },
+        'reminders': {
+        'useDefault': False,
+        },
+    }
+    event = service.events().insert(calendarId=calid, body=event).execute()
+    #return 'Event created: %s' % (event.get('htmlLink'))
+    locname = ""
+    for a in medcentres:
+        if a[0] == calid:
+            locname = a[1]
+    return "{\"status\": \"done\", \"loc\": \""+locname+"\"}"
 
         
 @app.after_request
