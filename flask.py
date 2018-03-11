f01346abd9ff9afad1be0b31e0cc46956c38d873
Souradip Mookerjee <souramoo@gmx.com>
2018-03-11 01:45:18 +0000

    tarted nhs choices data scraping integration

---
+++ flask.py
@@ -0,0 +1,16 @@
+
+from flask import Flask
+import requests, re
+
+app = Flask(__name__)
+
+@app.route("/p/<problem>")
+def hello(problem):
+    r = requests.get('https://www.nhs.uk/Search/?q='+problem)
+    p = re.compile("<li data-fb-result=https://www.nhs.uk/conditions/(.+?)>")
+    uu = "https://www.nhs.uk/conditions/" + p.search(r.content.decode("utf-8")).group(1)
+    r = requests.get(uu)
+    if "pharmacist can help" in r.content.decode("utf-8"):
+        return "1"
+    else:
+        return "0"
